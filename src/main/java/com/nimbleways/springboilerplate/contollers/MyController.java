3b6246f6daf162e79b365dbae605b76c711115d8
omarOMB <omar99.moustahib3@gmail.com>
2024-06-22 22:26:01 +0100

    code solution

---
+++ src/main/java/com/nimbleways/springboilerplate/contollers/MyController.java
@@ -0,0 +1,85 @@
+package com.nimbleways.springboilerplate.contollers;
+
+import com.nimbleways.springboilerplate.dto.product.ProcessOrderResponse;
+import com.nimbleways.springboilerplate.entities.Order;
+import com.nimbleways.springboilerplate.entities.Product;
+import com.nimbleways.springboilerplate.repositories.OrderRepository;
+import com.nimbleways.springboilerplate.repositories.ProductRepository;
+import com.nimbleways.springboilerplate.services.implementations.ProductService;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orders")
+public class MyController {
+    @Autowired
+    private ProductService ps;
+
+    @Autowired
+    private ProductRepository pr;
+
+    @Autowired
+    private OrderRepository or;
+
+    @PostMapping("{orderId}/processOrder")
+    @ResponseStatus(HttpStatus.OK)
+    public ProcessOrderResponse processOrder(@PathVariable Long orderId) {
+        Order order = or.findById(orderId).get();
+        System.out.println(order);
+        List<Long> ids = new ArrayList<>();
+        ids.add(orderId);
+        Set<Product> products = order.getItems();
+        for (Product p : products) {
+            if (p.getType().equals("NORMAL")) {
+                if (p.getAvailable() > 0) {
+                    p.setAvailable(p.getAvailable() - 1);
+                    pr.save(p);
+                } else {
+                    int leadTime = p.getLeadTime();
+                    if (leadTime > 0) {
+                        ps.notifyDelay(leadTime, p);
+                    }
+                }
+            } else if (p.getType().equals("SEASONAL")) {
+                // Add new season rules
+                if ((LocalDate.now().isAfter(p.getSeasonStartDate()) && LocalDate.now().isBefore(p.getSeasonEndDate())
+                        && p.getAvailable() > 0)) {
+                    p.setAvailable(p.getAvailable() - 1);
+                    pr.save(p);
+                } else {
+                    ps.handleSeasonalProduct(p);
+                }
+            } else if (p.getType().equals("EXPIRABLE")) {
+                if (p.getAvailable() > 0 && p.getExpiryDate().isAfter(LocalDate.now())) {
+                    p.setAvailable(p.getAvailable() - 1);
+                    pr.save(p);
+                } else {
+                    ps.handleExpiredProduct(p);
+                }
+            }else if (p.getType().equals("FLASHSALE")) {
+                //Here i used the expiry date just to gain time i can add a field in db as number of days
+                // in that way we can just add the number of days to the current date and then store it in expiry date
+                //that way we can always get expiry date based on number of dates we choosed for limitting
+                if (LocalDate.now().isBefore(p.getExpiryDate()) && p.getFlashsaleMaxQuantity()> 0) {
+                    p.setAvailable(p.getAvailable() - 1);
+                    pr.save(p);
+                } else {
+                    ps.handleFlashSaleProduct(p);
+                }
+            }
+        }
+
+        return new ProcessOrderResponse(order.getId());
+    }
+}
